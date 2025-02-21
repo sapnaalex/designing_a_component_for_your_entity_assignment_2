9e18ed21ba3b877eab5222a6096452945f4829da
sapnaalex <sapna.alex03@gmail.com>
2025-02-21 09:06:18 +0530

    designing_a_component_for_your_entity_assignment_2

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,10 @@
 // write button card here
+
+import React from "react";
+
+
+function ViewProduct(){
+    return <button style={{padding: "10px", background:"blue", color: "white", border:"none", cursor:"pointer"}}>Veiw Product</button>
+};
+
+export default ViewProduct;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,22 @@
 // write product card here
+import React from "react";
+import ViewProduct from "./button";
+
+
+function productCards(){
+    const productImage = "https://welpix.b-cdn.net/wp-content/uploads/2024/06/A-guide-to-skincare-product-photography-1536x876.webp";
+    const productName = "SkinCare";
+    const productPrice = "$50";
+
+    return(
+        <div style={{border:"1px solid #ccc", width:"200px", textAlign:"center", borderRadius:"8px"}}>
+            <img src={productImage} alt="Product" style={{width:"100%", borderRadius:"8px"}} />
+            <h3>{productName}</h3>
+            <p>{productPrice}</p>
+            <ViewProduct/>
+        </div>
+    );
+}
+
+export default productCards;
+
